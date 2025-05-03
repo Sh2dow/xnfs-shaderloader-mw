4a9e12c5a113aa26f14b762b16e69bcdd8dde983
sh2dow <2311420+Sh2dow@users.noreply.github.com>
2025-05-03 03:46:03 +0300

    some progress on fx loading

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,8 +1,4 @@
-﻿// NFSMW Shader Compiler & Loader
-// Compiles and/or loads shaders outside the executable
-// Put HLSL effect .fx files in the fx folder in the game directory
-// Or put compiled shader objects with resource names next to executable
-
+﻿// NFSMW Shader Compiler & Loader (Trampoline Style)
 #include "stdafx.h"
 #include "stdio.h"
 #include <windows.h>
@@ -15,105 +11,86 @@ ID3DXEffectCompiler* pEffectCompiler;
 ID3DXBuffer* pBuffer, *pEffectBuffer;
 char FilenameBuf[2048];
 
-char* ErrorString;
-
 bool bConsoleExists(void)
 {
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-
-	if (!GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
-		return false;
-
-	return true;
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    return GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
 }
 
-// custom print methods which include console attachment checks...
 int __cdecl cusprintf(const char* Format, ...)
 {
-	va_list ArgList;
-	int Result = 0;
-
-	if (bConsoleExists())
-	{
-		__crt_va_start(ArgList, Format);
-		Result = vprintf(Format, ArgList);
-		__crt_va_end(ArgList);
-	}
-
-	return Result;
+    va_list ArgList;
+    int Result = 0;
+    if (bConsoleExists()) {
+        __crt_va_start(ArgList, Format);
+        Result = vprintf(Format, ArgList);
+        __crt_va_end(ArgList);
+    }
+    return Result;
 }
 
-int __cdecl cus_puts(char* buf)
+int __cdecl cus_puts(const char* buf)
 {
-	if (bConsoleExists())
-		return puts(buf);
-	return 0;
+    return bConsoleExists() ? puts(buf) : 0;
 }
 
-bool CheckIfFileExists(const char* FileName)
+HRESULT __stdcall D3DXCreateEffectFromResourceHook(
+    LPDIRECT3DDEVICE9 pDevice,
+    HMODULE hSrcModule,
+    LPCTSTR pSrcResource,
+    CONST D3DXMACRO* pDefines,
+    LPD3DXINCLUDE pInclude,
+    DWORD Flags,
+    LPD3DXEFFECTPOOL pPool,
+    LPD3DXEFFECT* ppEffect,
+    LPD3DXBUFFER* ppCompilationErrors)
 {
-	FILE *fin = fopen(FileName, "rb");
-	if (fin == NULL)
-	{
-		char secondarypath[MAX_PATH];
-		strcpy(secondarypath, "fx\\");
-		strcat(secondarypath, FileName);
-		fin = fopen(secondarypath, "rb");
-		if (fin == NULL)
-			return 0;
-		strcpy((char*)FileName, secondarypath);
-	}
-	fclose(fin);
-	return 1;
-}
+    char* LastDot;
+    char* FxFilePath = nullptr;
 
-bool WriteFileFromMemory(const char* FileName, const void* buffer, long size)
-{
-	FILE *fout = fopen(FileName, "wb");
-	if (fout == NULL)
-		return 0;
+    DWORD shaderIndex;
+    __asm mov shaderIndex, edx;
+    CurrentShaderNum = shaderIndex;
+    cusprintf("Shader index received: %u\n", shaderIndex);
 
-	fwrite(buffer, 1, size, fout);
+    uintptr_t tableBase = 0x008F9BE8;
+    uintptr_t entryOffset = shaderIndex * 0x36;
 
-	fclose(fout);
-	return 1;
-}
+    __try {
+        if (shaderIndex >= 64)
+            throw 1;
 
-HRESULT __stdcall D3DXCreateEffectFromResourceHook(LPDIRECT3DDEVICE9 pDevice,
-	HMODULE           hSrcModule,
-	LPCTSTR           pSrcResource,
-	const D3DXMACRO         *pDefines,
-	LPD3DXINCLUDE     pInclude,
-	DWORD             Flags,
-	LPD3DXEFFECTPOOL  pPool,
-	LPD3DXEFFECT      *ppEffect,
-	LPD3DXBUFFER      *ppCompilationErrors
-)
-{
-    char* LastUnderline;
-	char* FxFilePath;
+        FxFilePath = *(char**)(tableBase + entryOffset);
+        if (!FxFilePath || IsBadReadPtr(FxFilePath, 4))
+            throw 2;
 
-	__try {
-		DWORD offset = (CurrentShaderNum + CurrentShaderNum * 8) << 4;
-		FxFilePath = *(char**)(0x008F9B60 + offset); // MW table location
-	} __except (EXCEPTION_EXECUTE_HANDLER) {
-		cusprintf("Invalid shader index: %u\n", CurrentShaderNum);
-		return E_FAIL;
-	}
-
-    strcpy(FilenameBuf, FxFilePath);
-    LastUnderline = strrchr(FilenameBuf, '.');
-    if (LastUnderline)
-    {
-        LastUnderline[1] = 'f';
-        LastUnderline[2] = 'x';
-        LastUnderline[3] = '\0';
+        cusprintf("Resolved shader path: %s\n", FxFilePath);
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER) {
+        char msg[128];
+        sprintf(msg, "Invalid shader index or null pointer (idx=%u)", shaderIndex);
+        MessageBoxA(NULL, msg, "Shader Loader Error", MB_ICONERROR);
+        return E_FAIL;
     }
 
-    if (GetFileAttributesA(FilenameBuf) != INVALID_FILE_ATTRIBUTES)
+    strncpy(FilenameBuf, FxFilePath, sizeof(FilenameBuf) - 1);
+    FilenameBuf[sizeof(FilenameBuf) - 1] = '\0';
+
+    LastDot = strrchr(FilenameBuf, '.');
+    if (LastDot)
     {
-    	HRESULT result;
-    	result = D3DXCreateEffectCompilerFromFile(FilenameBuf, NULL, NULL, 0, &pEffectCompiler, &pBuffer);
+        LastDot[1] = 'f';
+        LastDot[2] = 'x';
+        LastDot[3] = '\0';
+    }
+
+    cusprintf("Final FX path: %s\n", FilenameBuf);
+    DWORD fileAttr = GetFileAttributesA(FilenameBuf);
+    cusprintf("File exists: %s\n", (fileAttr != INVALID_FILE_ATTRIBUTES) ? "YES" : "NO");
+
+    if (fileAttr != INVALID_FILE_ATTRIBUTES)
+    {
+        HRESULT result = D3DXCreateEffectCompilerFromFile(FilenameBuf, NULL, NULL, 0, &pEffectCompiler, &pBuffer);
         if (SUCCEEDED(result))
         {
             cusprintf("Compiling shader %s\n", FilenameBuf);
@@ -130,14 +107,6 @@ HRESULT __stdcall D3DXCreateEffectFromResourceHook(LPDIRECT3DDEVICE9 pDevice,
                 return result;
             }
             cusprintf("Compilation successful!\n");
-
-        	// we better keep it in memory instead of writing to disk...
-
-        	//ppEffect = *(LPD3DXEFFECT*)(pEffectBuffer + 3);
-        	//CurrentEffectSize = *(unsigned int*)(pEffectBuffer + 2);
-        	//WriteFileFromMemory("temp_shader.cso", *(void**)(pEffectBuffer + 3), CurrentEffectSize);
-        	//result = D3DXCreateEffectFromFile(pDevice, "temp_shader.cso", pDefines, pInclude, Flags, pPool, ppEffect, &pBuffer);
-        	
             result = D3DXCreateEffect(pDevice, pEffectBuffer->GetBufferPointer(), pEffectBuffer->GetBufferSize(), pDefines, pInclude, Flags, pPool, ppEffect, &pBuffer);
             if (!SUCCEEDED(result))
             {
@@ -148,21 +117,18 @@ HRESULT __stdcall D3DXCreateEffectFromResourceHook(LPDIRECT3DDEVICE9 pDevice,
                     cus_puts(err);
                 }
             }
-        	//remove("temp_shader.cso");
-        	return result;
+            return result;
         }
-        else
+        if (pBuffer)
         {
-            cusprintf("Error compiling shader: HRESULT: %X\n", result);
-            if (pBuffer)
-            {
-                char* err = (char*)pBuffer->GetBufferPointer();
-                cus_puts(err);
-            }
+            char* err = (char*)pBuffer->GetBufferPointer();
+            MessageBoxA(NULL, err, "Shader Compilation Error", MB_ICONERROR);
+            cus_puts(err);
         }
+        cusprintf("Error compiling shader: HRESULT: %X\n", result);
     }
 
-	if (CheckIfFileExists(pSrcResource))
+    if (GetFileAttributesA(pSrcResource) != INVALID_FILE_ATTRIBUTES)
     {
         return D3DXCreateEffectFromFile(pDevice, pSrcResource, pDefines, pInclude, Flags, pPool, ppEffect, ppCompilationErrors);
     }
@@ -170,22 +136,31 @@ HRESULT __stdcall D3DXCreateEffectFromResourceHook(LPDIRECT3DDEVICE9 pDevice,
     return D3DXCreateEffectFromResource(pDevice, hSrcModule, pSrcResource, pDefines, pInclude, Flags, pPool, ppEffect, ppCompilationErrors);
 }
 
+__declspec(naked) void ShaderHookStub()
+{
+    __asm {
+        mov CurrentShaderNum, edx
+        push edx
+        call D3DXCreateEffectFromResourceHook
+        ret 0x24
+    }
+}
+
 int Init()
 {
-	injector::MakeCALL(0x006C60D2, D3DXCreateEffectFromResourceHook, true);
-	return 0;
+    injector::MakeCALL(0x006C60D2, ShaderHookStub, true);
+    return 0;
 }
 
 BOOL APIENTRY DllMain(HMODULE /*hModule*/, DWORD reason, LPVOID /*lpReserved*/)
 {
-	if (reason == DLL_PROCESS_ATTACH)
-	{
-		if (bConsoleExists())
-		{
-			freopen("CON", "w", stdout);
-			freopen("CON", "w", stderr);
-		}
-		Init();
-	}
-	return TRUE;
+    if (reason == DLL_PROCESS_ATTACH)
+    {
+        if (bConsoleExists()) {
+            freopen("CON", "w", stdout);
+            freopen("CON", "w", stderr);
+        }
+        Init();
+    }
+    return TRUE;
 }
